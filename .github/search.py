04fbe9689ee87493686769f8e3e9e4190b36194f
Majorvin Tan <majorvin.tan@mosaic.com>
2021-08-20 10:25:35 -0400

    test

---
+++ .github/search.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import os
+
+ticker = "XAW.TO"
+alphaToken = os.environ['alphaToken']
+
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey=$" + alphaToken
+r = requests.get(url)
+body = r.json()
+
+
+price = ""
+date = ""
+for x in body:
+    if "Time" in x:
+        for date in body[x]:
+            for p in body[x][date]:
+                if "close" in p:
+                    price = body[x][date][p]
+                    break
+            break
+
+
+closing_price = {
+    date: "" + price
+}
+
+with open("./data/" + ticker + ".json", "w") as file:
+    json.dump(closing_price, file)
